474bada97057a1782eb18ddfcdbcd0dbbf193b51
Jeffrey Platt <jsplatt@gmail.com>
2021-11-08 10:01:37 -0500

    getting tw to work

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import tw from "tailwind-styled-components"
+import tw from 'tailwind-styled-components'
 
 export default function Home() {
   return (
@@ -13,12 +12,12 @@ export default function Home() {
 }
 
 const Wrapper = tw.div`
-
+  flex flex-col bg-red-300
+`
+const Map = tw.div`
+  bg-red-500
 `
 const ActionItems = tw.div`
 
 `
 
-const Map = tw.div`
-
-`
