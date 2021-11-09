97d7e6de8ade2d91ac937700e7bd5dde46490ba3
Jeffrey Platt <jsplatt@gmail.com>
2021-11-09 10:24:13 -0500

    started map

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from 'tailwind-styled-components'
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from '!mapbox-gl'
+import { useEffect } from 'react'
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianNwbGF0dCIsImEiOiJja3ZzODB0Z3IwYXB0MnZwcHpqdGE1YTN4In0.h4i1YFRVuGN0e-yZ_m_9tg';
 
 export default function Home() {
+
+  useEffect(() =>{
+    console.log('hello')
+  }, [])
+  // const map = new mapboxgl.Map({
+  //   container: 'YOUR_CONTAINER_ELEMENT_ID',
+  //   style: 'mapbox://styles/mapbox/streets-v11',
+  //   center: ,
+  //   zoom: ,
+  //   });
+
   return (
     <Wrapper>
       <Map>Map</Map>
