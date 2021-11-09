3acb382db0b1e24e4a9ec33532c9a455c26239f5
Jeffrey Platt <jsplatt@gmail.com>
2021-11-09 11:07:21 -0500

    header almost done

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,45 @@ export default function Home() {
   return (
     <Wrapper>
       <Map />
-      <ActionItems>Start</ActionItems> 
+      <ActionItems>
+          {/* Header  */}
+          <Header>
+            <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+            <Profile>
+              <Name>JP</Name>
+              <UserImage
+                src="https://avatars.githubusercontent.com/u/74726047?v=4"
+              />
+            </Profile>
+          </Header>
+          {/* ActionButtons */}
+          {/* InputButton */}
+      
+      </ActionItems> 
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen 
+flex flex-col h-screen 
 `
 const ActionItems = tw.div`
-  flex-1
+flex-1 p-4
+`
+const Header = tw.div`
+flex justify-between items-center
+`
+const UberLogo = tw.img`
+h-28
+`
+const Profile = tw.div`
+flex items-center
 `
 
+const Name = tw.div`
+mr-4 w-20 text-sm
+`
+
+const UserImage = tw.img`
+h-12 w-12 rounded-full border-gray-200 p-px
+`
