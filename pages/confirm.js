8df833d381cf73bbeb609fbcf1dabd34c0dd5ba5
Jeffrey Platt <jsplatt@gmail.com>
2021-11-11 10:38:05 -0500

    pickup/dropoff cords pulling properly

--- pages/confirm.js
+++ pages/confirm.js
@@ -5,10 +5,10 @@ import { useEffect } from 'react'
 
 const Confirm = () => {
 
-    const getCoordinates = () => {
-        const location = ["Santa Monica"]
+    const getPickupCoordinates = () => {
+        const pickup = ["Santa Monica"]
 
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?`+
             new URLSearchParams({
                 access_token: "pk.eyJ1IjoianNwbGF0dCIsImEiOiJja3ZzODB0Z3IwYXB0MnZwcHpqdGE1YTN4In0.h4i1YFRVuGN0e-yZ_m_9tg",
                 limit: 1
@@ -20,8 +20,25 @@ const Confirm = () => {
         })
     }
 
+    const getDropOffCoordinates = () => {
+        const dropoff = ["Los Angeles"]
+
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?`+
+            new URLSearchParams({
+                access_token: "pk.eyJ1IjoianNwbGF0dCIsImEiOiJja3ZzODB0Z3IwYXB0MnZwcHpqdGE1YTN4In0.h4i1YFRVuGN0e-yZ_m_9tg",
+                limit: 1
+            })
+        )
+        .then(response => response.json())
+        .then(data => {
+            console.log("dropoff")
+            console.log(data.features[0].center)
+        })
+    }
+
     useEffect(()=>{
-        getCoordinates()
+        getPickupCoordinates()
+        getDropOffCoordinates()
     }, [])
 
     return (
